e5f4566ecc84308f0ceb2bb367b2653606006df5
gwarmstrong <garmstrong@colgate.edu>
2021-02-18 12:18:36 -0800

    ENH make artifact so datasets are more extensible

--- q2_anomaly_detection/datasets.py
+++ q2_anomaly_detection/datasets.py
@@ -1,3 +1,4 @@
+from typing import Dict
 from abc import ABC
 from urllib import request
 from zipfile import ZipFile
@@ -5,6 +6,7 @@ import os
 import pandas as pd
 from biom import load_table
 from q2_anomaly_detection.exceptions import DatasetError
+from http.client import HTTPResponse
 
 
 def download_and_extract_response(response, path):
@@ -45,11 +47,12 @@ class _FilesystemArchiver(QiitaSaveMixin):
 
     def __init__(self, dataset):
         self.dataset: Dataset = dataset
+        self.path = self.dataset.path
 
     @property
     def metadata_path(self):
         path_to_metadata = os.path.join(
-            self.dataset.path, self.metadata_kw, 'templates'
+            self.path, self.metadata_kw, 'templates'
         )
         if not os.path.exists(path_to_metadata):
             return False
@@ -87,41 +90,105 @@ class _FilesystemArchiver(QiitaSaveMixin):
     def table_exists(self):
         return os.path.exists(self.table_path)
 
+    def read(self, artifact: 'FileSystemArtifact'):
+        return artifact.filesystem_read(self)
+
+    def exists(self, artifact: 'FileSystemArtifact'):
+        return artifact.filesystem_exists(self)
+
+    def path(self, artifact: 'FileSystemArtifact'):
+        return artifact.filesystem_path(self)
+
 
 class Artifact:
     pass
 
 
-class QiitaArtifact:
+class QiitaArtifact(Artifact):
     _artifact_fstring = "https://qiita.ucsd.edu/public_artifact_download/" \
                         "?artifact_id={}"
     _metadata_fstring = "https://qiita.ucsd.edu/public_download/" \
                         "?data=sample_information&study_id={}"
 
+    def qiita_download(self, client: '_QiitaClient') -> HTTPResponse:
+        raise NotImplementedError('You should implement this.')
 
-class Table(Artifact, QiitaArtifact):
+
+class FileSystemArtifact:
+    def filesystem_exists(self, archiver: _FilesystemArchiver):
+        raise NotImplementedError('You should implement this.')
+
+    def filesystem_read(self, archiver: _FilesystemArchiver):
+        raise NotImplementedError('You should implement this.')
+
+    def filesystem_path(self, archiver: _FilesystemArchiver):
+        raise NotImplementedError('You should implement this.')
+
+
+class Table(Artifact, QiitaArtifact, FileSystemArtifact):
 
     def __init__(self, name, artifact_id):
         self.name = name
         self.artifact_id = artifact_id
 
-    def qiita_download(self, client: '_QiitaClient'):
+    def qiita_download(self, client: '_QiitaClient') -> HTTPResponse:
         table_link = self._artifact_fstring.format(self.artifact_id)
         r = client.make_request(table_link)
         return r
 
+    def filesystem_exists(self, archiver):
+        path = self.filesystem_path(archiver)
+        return os.path.exists(path)
 
-class Metadata(Artifact, QiitaArtifact):
+    def filesystem_read(self, archiver):
+        path = self.filesystem_path(archiver)
+        return load_table(path)
+
+    def filesystem_path(self, archiver):
+        path_to_table = os.path.join(
+            archiver.path, self.name, 'BIOM',
+            str(self.artifact_id),
+            'otu_table.biom',
+        )
+        return os.path.abspath(path_to_table)
+
+
+class Metadata(Artifact, QiitaArtifact, FileSystemArtifact):
 
     def __init__(self, name, study_id):
         self.name = name
         self.study_id = study_id
 
-    def qiita_download(self, client: '_QiitaClient'):
+    def qiita_download(self, client: '_QiitaClient') -> HTTPResponse:
         metadata_link = self._metadata_fstring.format(
             self.study_id)
         r = client.make_request(metadata_link)
         return r
+
+    def filesystem_exists(self, archiver):
+        return True if self.filesystem_path(archiver) else False
+
+    def filesystem_read(self, archiver):
+        path = self.filesystem_path(archiver)
+        return pd.read_csv(path, sep='\t')
+
+    def filesystem_path(self, archiver):
+        path_to_metadata = os.path.join(
+            archiver.path, self.name, 'templates'
+        )
+        if not os.path.exists(path_to_metadata):
+            return False
+        md_candidates = list(
+            filter(lambda n: n.endswith('.txt'),
+                   os.listdir(path_to_metadata)
+                   )
+        )
+        if len(md_candidates) == 0:
+            return False
+        else:
+            md_file = md_candidates[0]
+        full_path = os.path.join(path_to_metadata, md_file)
+        return os.path.abspath(full_path)
 
 
 class _QiitaClient:
@@ -146,6 +213,10 @@ class _QiitaClient:
         r = self.make_request(metadata_link)
         self.archiver.save_qiita_metadata(r)
 
+    def download(self, artifact: QiitaArtifact):
+        r = artifact.qiita_download(self)
+        self.archiver.save(artifact, r)
+
     @staticmethod
     def make_request(url):
         return request.urlopen(url)
@@ -155,6 +226,8 @@ class Dataset(ABC):
 
     study_id: int
     table_artifact_id: int
+
+    artifacts: Dict[Artifact]
 
     def __init__(self, path, download=True):
         self.path = path
@@ -170,19 +243,29 @@ class Dataset(ABC):
 
         self._table = None
         self._metadata = None
+        self._data = dict()
 
     def download(self):
         if self._check_integrity():
             print('Files already downloaded and verified')
             return
-        self.client.download_metadata()
-        self.client.download_table()
+        for artifact in self.artifacts.values():
+            self.client.download(artifact)
 
     def _check_integrity(self):
-        return all([
-                self.archiver.metadata_exists(),
-                self.archiver.table_exists(),
-            ])
+        return all(
+                self.archiver.exists(artifact) for artifact in
+                self.artifacts.values()
+            )
+
+    def __getitem__(self, item):
+        if item in self.artifacts and item not in self._data:
+            self._data[item] = self.archiver.read(self.artifacts[item])
+            return self._data[item]
+        else:
+            # will raise an exception if item not in self._data
+            # TODO maybe I should raise my own KeyError here
+            return self._data[item]
 
     @property
     def table(self):
@@ -229,6 +312,11 @@ class Dataset(ABC):
 class KeyboardDataset(Dataset):
     study_id = 232
     table_artifact_id = 46809
+
+    artifacts = {
+        'metadata': Metadata('metadata', study_id),
+        'table': Table('table', table_artifact_id),
+    }
 
 
 # class DietInterventionStudy(MultiPrepDataset):
