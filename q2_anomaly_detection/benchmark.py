3144a6aece8ab15011780f87ffcfa3688904a2c0
gwarmstrong <garmstrong@colgate.edu>
2021-02-24 13:38:44 -0800

    ENH more consolidation of benchmarking logic and conversion of plots to rank

--- q2_anomaly_detection/benchmark.py
+++ q2_anomaly_detection/benchmark.py
@@ -1,6 +1,39 @@
+import pandas as pd
 from sklearn.metrics import roc_auc_score, average_precision_score
-from q2_anomaly_detection.utils import as_dense
 from sklearn.preprocessing import MinMaxScaler
+from q2_anomaly_detection.utils import as_dense
+from q2_anomaly_detection.cross_validation import column_value_splitter
+from scipy.stats import rankdata
+
+
+class Results(list):
+
+    def __init__(self, results_dict):
+        super().__init__(results_dict)
+
+    def long_form(self):
+        res_df = pd.DataFrame.from_dict(
+            {(e['model_name'], e['category'], l['sample_id']):
+                 [l['score'], l['scaled_score'], l['score_rank'],
+                  l['train_test']]
+             for e in self
+             for l in e['anomaly_scores']
+             },
+            orient='index',
+            columns=['anomaly_score',
+                     'scaled_score',
+                     'score_rank',
+                     'train_test'],
+        )
+        res_df.index = pd.MultiIndex.from_tuples(res_df.index,
+                                                 names=['model_name',
+                                                        'category',
+                                                        'sample_id'])
+        return res_df
+
+    def short_form(self):
+        agg_results = pd.DataFrame(self).drop('anomaly_scores', axis=1)
+        return agg_results
 
 
 class Benchmark:
@@ -8,11 +41,17 @@ class Benchmark:
     def __init__(self, models):
         self.models = models
 
-    def benchmarking_loop(self, table, metadata, truth_category, splitter):
+    @staticmethod
+    def splitter(table, metadata, training_category):
+        return column_value_splitter(table, metadata, training_category)
+
+    def benchmarking_loop(self, table, metadata, truth_category,
+                          training_category):
         all_results = []
         for model_name, model_attrs in self.models.items():
             model = model_attrs['model']
-            for val, ids, training_table in splitter():
+            splitter = self.splitter(table, metadata, training_category)
+            for val, ids, training_table in splitter:
                 score_scaler = MinMaxScaler(clip=True)
                 # Training
                 model.fit(as_dense(training_table))
@@ -52,16 +91,18 @@ class Benchmark:
 
                 # assigning individual anomaly scores to samples in the results
                 # output
+                ranked_scores = rankdata(scores)
                 results['anomaly_scores'] = [
                     {'sample_id': id_, 'score': score,
-                     'scaled_score': scaled, 'train_test': 'train'
-                     if id_ in train_ids else 'test'} for
-                    id_, score, scaled in zip(
+                     'scaled_score': scaled,
+                     'score_rank': ranked_score,
+                     'train_test': 'train' if id_ in train_ids else 'test'} for
+                    id_, score, scaled, ranked_score in zip(
                         test_table.ids('sample'),
-                        scores, scores_scaled,
+                        scores, scores_scaled, ranked_scores
                     )
                 ]
 
                 all_results.append(results)
 
-        return all_results
+        return Results(all_results)
