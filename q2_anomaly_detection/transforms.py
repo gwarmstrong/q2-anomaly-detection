0e110f92250062bb6c55db8db1effbd4046d9477
gwarmstrong <garmstrong@colgate.edu>
2021-03-03 14:54:09 -0800

    NEW first draft of unifrac transformer

--- q2_anomaly_detection/transforms.py
+++ q2_anomaly_detection/transforms.py
@@ -1,7 +1,50 @@
+import tempfile
 import numpy as np
+from scipy.spatial.distance import cdist
 from sklearn.base import TransformerMixin
 from skbio.stats.composition import clr
 from skbio.stats import subsample_counts
+from skbio.diversity.beta import unweighted_unifrac
+from q2_anomaly_detection.utils import as_dense
+from functools import partial
+import pandas as pd
+from unifrac import ssu
+
+
+class AsDense(TransformerMixin):
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        return as_dense(X)
+
+
+class UniFrac(TransformerMixin):
+
+    def __init__(self, tree, **kneighbors_kwargs):
+        self.tree = tree
+        self.otu_ids = None
+        self.distance_fn = None
+        self.input_data = None
+
+    def fit(self, X, y=None):
+        """
+
+        X : pd.DataFrame
+            Must contain the OTU IDs in the column names
+
+        """
+        self.otu_ids = X.columns.values
+        self.distance_fn = partial(unweighted_unifrac, otu_ids=self.otu_ids,
+                                   tree=self.tree,
+                                   )
+        # TODO this may want to make a copy of X
+        self.input_data = X
+        return self
+
+    def transform(self, X):
+        return cdist(X, self.input_data, metric=self.distance_fn)
 
 
 class Rarefaction(TransformerMixin):
@@ -19,7 +62,12 @@ class Rarefaction(TransformerMixin):
         """
         Caution: this will return different results for the same sample
         """
-        X = np.delete(X, self.idx, axis=1)
+        if isinstance(X, pd.DataFrame):
+            idx = np.array([True] * len(X.columns))
+            idx[self.idx[:, 1]] = False
+            X = X.loc[:, idx]
+        else:
+            X = np.delete(X, self.idx, axis=1)
         X = self._subsample(X)
 
         return X
@@ -34,7 +82,8 @@ class Rarefaction(TransformerMixin):
     def _subsample(self, X):
         X = X.astype(int)
         X_out = list()
-        for row in X:
+        iter_var = X.values if isinstance(X, pd.DataFrame) else X
+        for row in iter_var:
             new_X = subsample_counts(row, n=self.depth, replace=self.replace)
             X_out.append(new_X)
         X = np.vstack(X_out)
